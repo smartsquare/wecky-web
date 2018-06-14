a4c52fbad82774522d4492de1ac3d017eff85d18
deen13 <dennis.dierkes@icloud.com>
2018-06-14 21:04:11 +0200

    GET websites from backend

--- src/main/kotlin/de/smartsquare/wecky/weckyweb/domain/WebsiteController.kt
+++ src/main/kotlin/de/smartsquare/wecky/weckyweb/domain/WebsiteController.kt
@@ -1,15 +1,16 @@
 package de.smartsquare.wecky.weckyweb.domain
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
 
 @RestController
 @RequestMapping
 class WebsiteController(private val websiteRepository: WebsiteRepository) {
 
+    @CrossOrigin
     @GetMapping("website")
     fun getAllWebsites() : MutableIterable<Website> = websiteRepository.findAll()
 
