35fcad10a1f54bb468f5e56ad4a76201294d088f
deen13 <dennis.dierkes@icloud.com>
2018-06-24 20:42:32 +0200

    Invoke wecky-crawl async
    this closes #1

--- src/main/kotlin/de/smartsquare/wecky/weckyweb/crawl/CrawlService.kt
+++ src/main/kotlin/de/smartsquare/wecky/weckyweb/crawl/CrawlService.kt
@@ -1,10 +1,12 @@
 package de.smartsquare.wecky.weckyweb.crawl
 
 import com.amazonaws.services.lambda.invoke.LambdaFunction
+import com.amazonaws.services.lambda.model.InvocationType.Event
 import de.smartsquare.wecky.weckyweb.domain.Website
 
 interface CrawlService {
 
-    @LambdaFunction(functionName = "awscodestar-wecky-crawl-lambda-GetHelloWorld-1CFXZAZA30GHG")
+    @LambdaFunction(functionName = "awscodestar-wecky-crawl-lambda-GetHelloWorld-1CFXZAZA30GHG", invocationType = Event)
     fun crawlWebsite(website: Website)
+
 }
