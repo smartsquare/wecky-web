3366f055a449a56399a78118ecb128676bfdaca1
Daniel Rosowski <rosowski@smartsquare.de>
2018-07-06 15:39:16 +0200

    Make bootstrapping optional

--- src/main/kotlin/de/smartsquare/wecky/weckyweb/WeckyWebApplication.kt
+++ src/main/kotlin/de/smartsquare/wecky/weckyweb/WeckyWebApplication.kt
@@ -14,33 +14,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
-import java.lang.Exception
 import java.util.*
 
 @SpringBootApplication
 @EnableScheduling
 class WeckyWebApplication {
 
-    private val log = LoggerFactory.getLogger(WeckyWebApplication::class.java)
+    companion object Factory {
+        val log = LoggerFactory.getLogger(WeckyWebApplication::class.java.simpleName)
+        val bootstrapKey = "BOOTSTRAP"
+    }
 
     @Bean
     fun init(amazonDynamoDB: AmazonDynamoDB, userRepo: UserRepository, websiteRepo: WebsiteRepository) = CommandLineRunner {
-        try {
-            createTable(amazonDynamoDB, Website::class.java)
-            createTable(amazonDynamoDB, User::class.java)
-            val userId = UUID.randomUUID().toString()
-            userRepo.save(User(userId, "Max Musterman", "max@muster.man"))
-            websiteRepo.save(Website("HEISE", "https://www.heise.de", userId))
-
-            // fetch all websites
-            log.info("Websites found with findAll():")
-            log.info("-------------------------------")
-            websiteRepo.findAll().forEach { log.info(it.toString()) }
-            log.info("")
-        } catch (ex: Exception) {
-            log.error("Error initializing!", ex)
+        if (!bootstrap()) {
+            return@CommandLineRunner
         }
+
+        createTable(amazonDynamoDB, Website::class.java)
+        createTable(amazonDynamoDB, User::class.java)
+        val userId = UUID.randomUUID().toString()
+        userRepo.save(User(userId, "Success Simulator", "success@simulator.amazonses.com"))
+        websiteRepo.save(Website("HEISE", "https://www.heise.de", userId))
+
+        // fetch all users
+        log.info("Users found with findAll():")
+        log.info("-------------------------------")
+        userRepo.findAll().forEach { log.info(it.toString()) }
+        log.info("")
+
+        // fetch all websites
+        log.info("Websites found with findAll():")
+        log.info("-------------------------------")
+        websiteRepo.findAll().forEach { log.info(it.toString()) }
+        log.info("")
     }
+
+    private fun bootstrap(): Boolean = (System.getenv(bootstrapKey)?.equals("true", true)
+            ?: System.getProperty(bootstrapKey)?.equals("true", true)) ?: false
 
     private fun createTable(amazonDynamoDB: AmazonDynamoDB, clazz: Class<out Any>) {
         val dynamoDBMapper = DynamoDBMapper(amazonDynamoDB)
