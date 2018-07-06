d3c356a45d125d77dfea1a1420dba7bdfe53ad97
Daniel Rosowski <rosowski@smartsquare.de>
2018-07-06 15:38:59 +0200

    Add support for local dynamodb

--- src/main/kotlin/de/smartsquare/wecky/weckyweb/domain/DynamoDbConfig.kt
+++ src/main/kotlin/de/smartsquare/wecky/weckyweb/domain/DynamoDbConfig.kt
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import org.slf4j.LoggerFactory
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -15,6 +16,11 @@ import org.springframework.context.annotation.Configuration
 @EnableDynamoDBRepositories
 (basePackages = arrayOf("de.smartsquare.wecky.weckyweb"))
 class DynamoDbConfig {
+
+    companion object Factory {
+        val log = LoggerFactory.getLogger(DynamoDbConfig::class.java.simpleName)
+    }
+
     @Value("\${amazon.dynamodb.endpoint}")
     val amazonDynamoDBEndpoint: String? = null
 
@@ -32,9 +38,15 @@ class DynamoDbConfig {
         System.setProperty("aws.accessKeyId", amazonAWSAccessKey)
         System.setProperty("aws.secretKey", amazonAWSSecretKey)
 
-        return AmazonDynamoDBClient.builder()
-                .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, amazonAWSRegion))
-                .build()
+        if (amazonDynamoDBEndpoint.isNullOrBlank()) {
+            log.info("Using production DynamoDB endpoint [$amazonAWSRegion]")
+            return AmazonDynamoDBClient.builder().withRegion(amazonAWSRegion).build()
+        } else {
+            log.info("Using local dev DynamoDB endpoint [$amazonDynamoDBEndpoint] [$amazonAWSRegion]")
+            return AmazonDynamoDBClient.builder()
+                    .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, amazonAWSRegion))
+                    .build()
+        }
     }
 
     @Bean
